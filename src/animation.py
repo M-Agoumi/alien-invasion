a0b849e99a209b32110299e4cd74d6c8944ab01a
magoumi <agoumi@mobiblanc.com>
2023-11-16 12:21:49 +0100

    add explosion animation

---
+++ src/animation.py
@@ -0,0 +1,38 @@
+import time
+from src.effect.explosion import Explosion
+
+class Animation():
+    """ class to handle all animations of our game """
+
+    def __init__(self, setting) -> None:
+        self.setting = setting
+        self.explosions = []
+        self.images = {}
+
+    
+    def add_explosion(self, x, y):
+        # load the image if it's not loaded
+        if 'explosion' not in self.images:
+            self.images['explosion'] = self.setting.game.image.load("resources/images/explosion/animation.bmp")
+        explosion = Explosion(self.setting, x, y, self.images['explosion'])
+        self.explosions.append(explosion)
+    
+    def draw_all(self):
+        self.draw_explosions()
+
+    
+    def draw_explosions(self):
+        current_time = int(time.time() * 1000)
+        explosions_to_remove = []
+
+        for explosion in self.explosions:
+            # check if expired
+            if (current_time - explosion.get_created_at()) >= 2000 :
+                explosions_to_remove.append(explosion)
+            else:
+                explosion.update()
+                explosion.draw()
+
+        # Remove expired explosions
+        for explosion in explosions_to_remove:
+            self.explosions.remove(explosion)
--- src/effect/explosion.py
+++ src/effect/explosion.py
@@ -1,5 +1,37 @@
 import time
 
-class Explosion():
-    ''' class for the explosion animation for the alien ship destruction'''
-    
+class Explosion:
+    ''' Class for the explosion animation for the alien ship destruction'''
+    def __init__(self, setting, x, y, image):
+        # Load the sprite sheet for the explosion animation
+        self.setting = setting
+        self.sheet = image
+        self.frame_width = 250
+        self.frame_height = 250
+        self.scaled_frame_size = (75, 75)
+        self.num_frames = 45  # Total number of frames
+        self.frames_per_row = self.num_frames  # Assuming all frames in a single row
+        self.frame_duration = 50  # 50 milliseconds per frame
+        self.current_frame = 0
+        self.x = x
+        self.y = y
+        self.created = time.time() * 1000  # Store creation time in milliseconds
+
+    def get_created_at(self):
+        return self.created
+
+    def draw(self):
+        frame_col = self.current_frame % self.frames_per_row
+        frame_rect = self.setting.game.Rect(frame_col * self.frame_width, 0, self.frame_width, self.frame_height)
+        frame_image = self.sheet.subsurface(frame_rect)
+        scaled_frame = self.setting.game.transform.scale(frame_image, self.scaled_frame_size)
+        self.setting.screen.blit(scaled_frame, (self.x, self.y))
+
+    def update(self):
+        elapsed_time = int(time.time() * 1000) - self.created
+        # Calculate the current frame based on elapsed time and frame duration
+        self.current_frame = min(self.num_frames - 1, elapsed_time // self.frame_duration)
+
+    def is_finished(self):
+        # Check if the animation has finished playing
+        return self.current_frame == self.num_frames - 1
